4fd9bf351cb4682b8ad1be16dd20043cf4413961
Pierre Baron <pierre.r.baron@gmail.com>
2022-03-10 14:56:16 +0100

    Added torchlight.dev as syntax highlighters

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const { DateTime } = require("luxon");
 const fs = require("fs");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+// const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
